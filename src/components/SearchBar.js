3515b2f27b805a63d8fea7bf28fdf034e1d2ef1a
Aric Hylton <Ahylton@student.fullsail.edu>
2023-02-08 22:33:34 -0600

    updated ratings display to allow users to better understand the maximum rating a movie could recieve

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,32 +4,32 @@ import { connect } from 'react-redux';
 import history from '../history';
 
 class SearchBar extends React.Component {
-	state = { term: '' };
+  state = { term: '' };
 
-	onFormSubmit = (event) => {
-		event.preventDefault();
-		this.props.loading(true);
-		this.props.searchMovies(this.state.term);
-		history.push('/search');
-	};
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.loading(true);
+    this.props.searchMovies(this.state.term);
+    history.push('/search');
+  };
 
-	render() {
-		return (
-			<form
-				className="ui form inverted item"
-				style={{ margin: 'auto', maxWidth: '900px' }}
-				onSubmit={this.onFormSubmit}
-			>
-				<input				
-					value={this.state.term}
-					onChange={(e) => this.setState({ term: e.target.value })}
-					required					
-					type="text"
-					placeholder="Search..."
-				/>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form
+        className='ui form inverted item'
+        style={{ margin: 'auto', maxWidth: '900px' }}
+        onSubmit={this.onFormSubmit}
+      >
+        <input
+          value={this.state.term}
+          onChange={(e) => this.setState({ term: e.target.value })}
+          required
+          type='text'
+          placeholder='Search...'
+        />
+      </form>
+    );
+  }
 }
 
 export default connect(null, { searchMovies, loading, searchTerm })(SearchBar);
