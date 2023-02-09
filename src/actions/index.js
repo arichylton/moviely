0694fe689037da829afd559108cade4110179f42
Aric Hylton <arich212@gmail.com>
2023-02-09 01:21:37 -0600

    updated npm packages

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,94 +2,89 @@ import axios from 'axios';
 import history from '../history';
 
 export const requestAuthToken = () => async (dispatch) => {
-	const response = await axios.get(
-		'https://api.themoviedb.org/3/authentication/token/new?api_key=3e17762214fd39739a9e5f272003618d'
-	);
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/authentication/token/new?api_key=3e17762214fd39739a9e5f272003618d'
+  );
 
-	dispatch({ type: 'SIGN_IN', payload: { ...response.data } });
+  dispatch({ type: 'SIGN_IN', payload: { ...response.data } });
 };
 
 export const guestSignIn = () => async (dispatch) => {
-	const response = await axios.get(
-		'https://api.themoviedb.org/3/authentication/guest_session/new?api_key=3e17762214fd39739a9e5f272003618d'
-	);
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/authentication/guest_session/new?api_key=3e17762214fd39739a9e5f272003618d'
+  );
 
-	dispatch({ type: 'GUEST_SIGN_IN', payload: { ...response.data } });
+  dispatch({ type: 'GUEST_SIGN_IN', payload: { ...response.data } });
 };
 
 export const signOut = () => {
-	return {
-		type: 'SIGN_OUT'
-	};
+  return {
+    type: 'SIGN_OUT',
+  };
 };
 
 export const findMovie = (id) => async (dispatch) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=3e17762214fd39739a9e5f272003618d&language=en-US`
+  );
 
-	const response = await axios.get(
-		`https://api.themoviedb.org/3/movie/${id}?api_key=3e17762214fd39739a9e5f272003618d&language=en-US`
-	);
+  await dispatch({ type: 'FIND_MOVIE', payload: response.data });
 
-	await dispatch({ type: 'FIND_MOVIE', payload: response.data });
-
-	history.push(`/movie/${id}`);
+  history.push(`/movie/${id}`);
 };
 
 export const featuredMovies = () => async (dispatch) => {
-	const response = await axios.get(
-		'https://api.themoviedb.org/3/trending/movie/week?api_key=3e17762214fd39739a9e5f272003618d'
-	);
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/trending/movie/week?api_key=3e17762214fd39739a9e5f272003618d'
+  );
 
-
-	dispatch({ type: 'FEATURED_MOVIES', payload: response.data.results });
+  dispatch({ type: 'FEATURED_MOVIES', payload: response.data.results });
 };
 
 export const highestRatedMovies = () => async (dispatch) => {
-	const response = await axios.get(
-		'https://api.themoviedb.org/3/movie/top_rated?api_key=3e17762214fd39739a9e5f272003618d&language=en-US&page=1'
-	);
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/movie/top_rated?api_key=3e17762214fd39739a9e5f272003618d&language=en-US&page=1'
+  );
 
-
-	dispatch({ type: 'HIGHEST_MOVIES', payload: response.data.results });
+  dispatch({ type: 'HIGHEST_MOVIES', payload: response.data.results });
 };
 
 export const latestMoviesData = () => async (dispatch) => {
-	const response = await axios.get(
-		'https://api.themoviedb.org/3/movie/upcoming?api_key=3e17762214fd39739a9e5f272003618d&language=en-US&page=1'
-	);
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/movie/upcoming?api_key=3e17762214fd39739a9e5f272003618d&language=en-US&page=1'
+  );
 
-
-	dispatch({ type: 'LATEST_MOVIES', payload: response.data.results });
+  dispatch({ type: 'LATEST_MOVIES', payload: response.data.results });
 };
 
 export const searchMovies = (input) => async (dispatch) => {
-	const response = await axios.get(
-		`https://api.themoviedb.org/3/search/movie?api_key=3e17762214fd39739a9e5f272003618d&language=en-US&query=${input}&page=1&include_adult=false`
-	);
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?api_key=3e17762214fd39739a9e5f272003618d&language=en-US&query=${input}&page=1&include_adult=false`
+  );
 
-	dispatch(loading(false));
-	dispatch(searchTerm(input));
+  dispatch(loading(false));
+  dispatch(searchTerm(input));
 
-	dispatch({ type: 'SEARCH_MOVIES', payload: response.data.results });
+  dispatch({ type: 'SEARCH_MOVIES', payload: response.data.results });
 };
 
-
 export const route = (data) => {
-	return {
-		type: 'ROUTE',
-		payload: data
-	};
+  return {
+    type: 'ROUTE',
+    payload: data,
+  };
 };
 
 export const searchTerm = (input) => {
-	return {
-		type: 'SEARCH_TERM',
-		payload: input
-	};
+  return {
+    type: 'SEARCH_TERM',
+    payload: input,
+  };
 };
 
 export const loading = (boolean) => {
-	return {
-		type: 'LOADING',
-		payload: boolean
-	};
+  return {
+    type: 'LOADING',
+    payload: boolean,
+  };
 };
--- src/components/pageComponents/RatingHearts.js
+++ src/components/pageComponents/RatingHearts.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Rating } from 'semantic-ui-react'
+import { Rating } from 'semantic-ui-react';
 
 class RatingHearts extends Component {
-  state = {}
+  state = {};
 
-  handleRate = (e, { rating, maxRating }) => this.setState({ rating, maxRating })
+  handleRate = (e, { rating, maxRating }) =>
+    this.setState({ rating, maxRating });
 
   render() {
     return (
       <div>
-        <Rating icon='heart' maxRating={10}  rating={this.props.movieData.vote_average} onRate={this.handleRate} disabled />
+        <Rating
+          icon='heart'
+          maxRating={10}
+          rating={this.props.movieData.vote_average}
+          onRate={this.handleRate}
+          disabled
+        />
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return { movieData: state.movieData }
-}
+const mapStateToProps = (state) => {
+  return { movieData: state.movieData };
+};
 
 export default connect(mapStateToProps)(RatingHearts);
